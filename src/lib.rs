4522d404c4c30e9c1093914d6d6ae7faa49b5369
OpenByte <development.openbyte@gmail.com>
2021-08-01 20:15:55 +0200

    Add u16str macro for creating non-terminated strings

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,10 @@ macro_rules! u16cstr {
         unsafe { $crate::widestring::U16CStr::from_slice_with_nul_unchecked($crate::wchar::wchz!(u16, $expression)) }
     };
 }
+
+#[macro_export]
+macro_rules! u16str {
+    ($expression:expr) => {
+        unsafe { $crate::widestring::U16Str::from_slice($crate::wchar::wch!(u16, $expression)) }
+    };
+}
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,7 +1,12 @@
-use u16cstr::u16cstr;
-use widestring::U16CString;
+use u16cstr::{u16cstr, u16str};
+use widestring::{U16CString, U16String};
 
 #[test]
-fn basic() {
+fn u16cstr() {
     assert_eq!(u16cstr!("Test"), U16CString::from_str("Test").unwrap().as_ref());
+}
+
+#[test]
+fn u16str() {
+    assert_eq!(u16str!("Test"), U16String::from_str("Test").as_ref());
 }
