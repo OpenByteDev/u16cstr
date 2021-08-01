227c297f0adab4e2ffdb6142ab90475826f7b380
OpenByte <development.openbyte@gmail.com>
2021-08-01 20:49:38 +0200

    Fix path in tests

--- tests/basic_in_another_crate.rs
+++ tests/basic_in_another_crate.rs
@@ -3,7 +3,7 @@ use std::{error::Error, path::PathBuf, process::Command, str::FromStr};
 #[test]
 fn basic_in_another_crate() -> Result<(), Box<dyn Error>> {
     let crate_name = "test-crate";
-    let crate_path = PathBuf::from_str(".\\tests")?.join(crate_name).canonicalize()?;
+    let crate_path = PathBuf::from_str("tests")?.join(crate_name).canonicalize()?;
 
     Command::new("cargo")
         .arg("test")
