0320968fa5fd31719f81e8930d878b5b78b40379
OpenByte <development.openbyte@gmail.com>
2021-08-01 20:36:57 +0200

    Make macro truly const

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,20 @@ pub extern crate wchar;
 
 #[macro_export]
 macro_rules! u16cstr {
-    ($expression:expr) => {
-        unsafe { $crate::widestring::U16CStr::from_slice_with_nul_unchecked($crate::wchar::wchz!(u16, $expression)) }
-    };
+    ($expression:expr) => {{
+        // the following would be nice to use but it is sadly not const.
+        // unsafe { $crate::widestring::U16CStr::from_slice_with_nul_unchecked($crate::wchar::wchz!(u16, $expression)) }
+
+        unsafe { ::core::mem::transmute::<&'static [u16], &'static $crate::widestring::U16CStr>($crate::wchar::wchz!(u16, $expression)) }
+    }};
 }
 
 #[macro_export]
 macro_rules! u16str {
-    ($expression:expr) => {
-        unsafe { $crate::widestring::U16Str::from_slice($crate::wchar::wch!(u16, $expression)) }
-    };
+    ($expression:expr) => {{
+        // the following would be nice to use but it is sadly not const.
+        // unsafe { $crate::widestring::U16Str::from_slice($crate::wchar::wch!(u16, $expression)) }
+
+        unsafe { ::core::mem::transmute::<&'static [u16], &'static $crate::widestring::U16Str>($crate::wchar::wch!(u16, $expression)) }
+    }};
 }
